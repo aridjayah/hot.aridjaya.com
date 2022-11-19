69ddfa14ce09c8d516dcc503e2999ab12eab7fba
aridjayawallpaper <irawanria85@gmail.com>
2022-11-19 12:45:40 +0700

    !

--- min.js
+++ min.js
@@ -1,29 +1,6 @@
 thumbnail();
 
 function thumbnail() {
-  document.getElementById('imageText').innerHTML = `
-  <div class="mcimg_v">
-  <div class="image-container">
-      <div class="top-info">
-          <label class="count-view">
-              <i class="far fa-eye">
-              </i>
-              <span style="padding-left: 6px;" id="view"> 88,719,976
-              </span>
-          </label>
-      </div>
-      <img src="./img/01.png" class="mcimg_v" width="492" height="328" data-i="8478650">
-  </div>
-</div>
-<div class="text-elements">
-  <div class="text_on_hover">
-      <div class="mctitle">
-          <a target="_blank"> Asian amateur videos
-          </a>
-      </div>
-  </div>
-</div>`;
-
-  document.getElementById('thumb').innerHTML = objetos[a];
+  document.getElementById('image-thumb').innerHTML = objetos[a];
   document.getElementById('title').innerHTML = citas[alea];
 }
